526757d1fc462e30a816d6aac208231a5221804e
hoangtriet <trietpro127hoangtu@gmail.com>
2019-10-22 13:57:16 +0700

    mới

---
+++ QuanLyNhaHang/Infrastructure/Persistence/Repositories/Repository.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Text;
+using ApplicationCore.Interfaces;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Persistence.Repositoties {
+    class Repository<T> : IRepository<T> where T : class {
+        protected DbContext Context;
+        public Repository (DbContext context) {
+            this.Context = context;
+        }
+
+        public void Add (T entity) {
+            Context.Add (entity);
+        }
+
+        public void AddRange (IEnumerable<T> entities) {
+            Context.AddRange (entities);
+        }
+
+        public IEnumerable<T> Find (Expression<Func<T, bool>> predicate) {
+            throw new NotImplementedException ();
+        }
+
+        public IEnumerable<T> GetAll () {
+            return Context.Set<T> ().ToList ();
+        }
+
+        public T GetById (int id) {
+            return Context.Set<T> ().Find (id);
+        }
+
+        public void Remove (T entity) {
+            Context.Set<T> ().Remove (entity);
+        }
+
+        public void RemoveRange (IEnumerable<T> entities) {
+            Context.Set<T> ().RemoveRange (entities);
+        }
+    }
+}
