900787ccf97f90c94610e696df06afc3783cf9f1
hoangtriet <trietpro127hoangtu@gmail.com>
2019-10-30 19:56:29 +0700

    máy đúng lag

--- QuanLyNhaHang/Infrastructure/Persistence/Data/SeedData.cs
+++ QuanLyNhaHang/Infrastructure/Persistence/Data/SeedData.cs
@@ -49,14 +49,226 @@ namespace Infrastructure.Persistence.Data {
                     }
                 );
             }
+
+            if (!context.ThucDons.Any ()) {
+                context.ThucDons.AddRange (
+                    new ThucDon {
+                        IdLoaiMonAn = 1,
+                            Ten = "Gỏi sứa",
+                            Gia = 60000
+                    },
+                    new ThucDon {
+                        IdLoaiMonAn = 1,
+                            Ten = "Cua rang me",
+                            Gia = 80000
+                    },
+                    new ThucDon {
+                        IdLoaiMonAn = 1,
+                            Ten = "Cua hấp sả",
+                            Gia = 200000
+                    },
+                    new ThucDon {
+                        IdLoaiMonAn = 1,
+                            Ten = "Cua rang muối",
+                            Gia = 250000
+                    },
+                    new ThucDon {
+                        IdLoaiMonAn = 1,
+                            Ten = "Sò huyết nướng",
+                            Gia = 250000
+                    },
+                    new ThucDon {
+                        IdLoaiMonAn = 2,
+                            Ten = "Mực xào thập cẩm",
+                            Gia = 280000
+                    },
+                    new ThucDon {
+                        IdLoaiMonAn = 2,
+                            Ten = "Mực chiên bơ",
+                            Gia = 280000
+                    },
+                    new ThucDon {
+                        IdLoaiMonAn = 2,
+                            Ten = "Mực trứng hấp",
+                            Gia = 180000
+                    },
+                    new ThucDon {
+                        IdLoaiMonAn = 2,
+                            Ten = "Mực trứng nướng muối ớt",
+                            Gia = 180000
+                    },
+                    new ThucDon {
+                        IdLoaiMonAn = 2,
+                            Ten = "Mực trứng chiên",
+                            Gia = 180000
+                    },
+                    new ThucDon {
+                        IdLoaiMonAn = 3,
+                            Ten = "Súp cá hồi",
+                            Gia = 20000
+                    },
+                    new ThucDon {
+                        IdLoaiMonAn = 3,
+                            Ten = "Gỏi cá hồi",
+                            Gia = 250000
+                    },
+                    new ThucDon {
+                        IdLoaiMonAn = 3,
+                            Ten = "Cháo cá hồi",
+                            Gia = 20000
+                    },
+                    new ThucDon {
+                        IdLoaiMonAn = 3,
+                            Ten = "Cá hồi nướng",
+                            Gia = 320000
+                    },
+                    new ThucDon {
+                        IdLoaiMonAn = 3,
+                            Ten = "Cá hồi hấp xì dầu",
+                            Gia = 350000
+                    },
+                    new ThucDon {
+                        IdLoaiMonAn = 3,
+                            Ten = "Cá hồi rang muối ",
+                            Gia = 320000
+                    },
+                    new ThucDon {
+                        IdLoaiMonAn = 4,
+                            Ten = "Súp cua ",
+                            Gia = 28000
+                    },
+                    new ThucDon {
+                        IdLoaiMonAn = 4,
+                            Ten = "Súp bắp cua ",
+                            Gia = 31000
+                    },
+                    new ThucDon {
+                        IdLoaiMonAn = 4,
+                            Ten = "Súp tôm ",
+                            Gia = 33000
+                    },
+                    new ThucDon {
+                        IdLoaiMonAn = 4,
+                            Ten = "Súp nghêu ",
+                            Gia = 28000
+                    },
+                    new ThucDon {
+                        IdLoaiMonAn = 4,
+                            Ten = "Súp hải sản ",
+                            Gia = 29000
+                    },
+                    new ThucDon {
+                        IdLoaiMonAn = 5,
+                            Ten = "Gỏi bưởi ",
+                            Gia = 121000
+                    },
+                    new ThucDon {
+                        IdLoaiMonAn = 5,
+                            Ten = "Gỏi bò bóp thấu ",
+                            Gia = 110000
+                    },
+                    new ThucDon {
+                        IdLoaiMonAn = 5,
+                            Ten = "Gỏi ngó sen tôm thịt ",
+                            Gia = 121000
+                    },
+                    new ThucDon {
+                        IdLoaiMonAn = 5,
+                            Ten = "Gỏi khoai môn ",
+                            Gia = 110000
+                    },
+                    new ThucDon {
+                        IdLoaiMonAn = 5,
+                            Ten = "Gỏi xoài tôm thịt ",
+                            Gia = 110000
+                    },
+                    new ThucDon {
+                        IdLoaiMonAn = 6,
+                            Ten = "Lẩu vịt ",
+                            Gia = 400000
+                    },
+                    new ThucDon {
+                        IdLoaiMonAn = 6,
+                            Ten = "Lẩu vịt ",
+                            Gia = 400000
+                    },
+                    new ThucDon {
+                        IdLoaiMonAn = 6,
+                            Ten = "Lẩu riêu cua hấp bò",
+                            Gia = 500000
+                    },
+                    new ThucDon {
+                        IdLoaiMonAn = 6,
+                            Ten = "Lẩu bò nhúng dấm ",
+                            Gia = 350000
+                    },
+                    new ThucDon {
+                        IdLoaiMonAn = 6,
+                            Ten = "Lẩu thập cẩm ",
+                            Gia = 600000
+                    },
+                    new ThucDon {
+                        IdLoaiMonAn = 6,
+                            Ten = "Lẩu ếch măng cay ",
+                            Gia = 400000
+                    },
+                    new ThucDon {
+                        IdLoaiMonAn = 6,
+                            Ten = "Lẩu gà rượu vang ",
+                            Gia = 600000
+                    },
+                    new ThucDon {
+                        IdLoaiMonAn = 7,
+                            Ten = "Chả giò thăng hoa ",
+                            Gia = 50000
+                    },
+                    new ThucDon {
+                        IdLoaiMonAn = 7,
+                            Ten = "Khoai lang chiên",
+                            Gia = 30000
+                    },
+                    new ThucDon {
+                        IdLoaiMonAn = 7,
+                            Ten = "Cà tím chiên dòn ",
+                            Gia = 40000
+                    },
+                    new ThucDon {
+                        IdLoaiMonAn = 7,
+                            Ten = "Hoành thánh chiên tứ vị ",
+                            Gia = 50000
+                    },
+                    new ThucDon {
+                        IdLoaiMonAn = 7,
+                            Ten = "Nấm bào ngư chiên xù ",
+                            Gia = 40000
+                    },
+                    new ThucDon {
+                        IdLoaiMonAn = 8,
+                            Ten = "Nước ngọt các loại ",
+                            Gia = 12000
+                    },
+                    new ThucDon {
+                        IdLoaiMonAn = 8,
+                            Ten = "Nước suối ",
+                            Gia = 7000
+                    },
+                    new ThucDon {
+                        IdLoaiMonAn = 8,
+                            Ten = "Bia các loại ",
+                            Gia = 15000
+                    },
+                );
+            }
             context.SaveChanges ();
+
         }
 
         private static string GetMd5Hash (MD5 md5, string input) {
             byte[] bytes = md5.ComputeHash (Encoding.UTF8.GetBytes (input));
             StringBuilder s = new StringBuilder ();
             for (int i = 0; i < bytes.Length; i++) {
-                s.Append (bytes[i].ToString ("X2"));
+                s.Append (bytes[i].ToString ("
+                                    X2 "));
 
             }
             return s.ToString ();
