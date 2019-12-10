f9d2c65c807f78dc6c9ce46112bfaed5fbe518fa
viethoanggg <49758848+viethoanggg@users.noreply.github.com>
2019-12-10 13:38:35 +0700

    kk

--- QuanLyNhaHang/ApplicationCore/Specification/ThucDonSpecification.cs
+++ QuanLyNhaHang/ApplicationCore/Specification/ThucDonSpecification.cs
@@ -24,30 +24,18 @@ namespace ApplicationCore.Specification
                 tenMonAn = "";
             if (giaTu > 0)
             {
-                // Expression<Func<ThucDon, bool>> giaExp = s => s.Gia >= giaTu;
-                // predicate = NoiExpression(predicate, giaExp);
                 predicate = s => s.Ten.ToLower().Contains(tenMonAn.ToLower()) && s.Gia >= giaTu;
-                //predicate = s => s.Gia >= giaTu;
             }
             if (giaDen != 0)
             {
                 predicate = s => s.Ten.ToLower().Contains(tenMonAn.ToLower()) && s.Gia >= giaTu && s.Gia <= giaDen;
-
             }
-            //if ()
-            // if (giaDen != 0)
-            // {
-            //     //Expression<Func<ThucDon, bool>> giaExp = s => s.Gia <= giaDen;
-            //     // predicate = NoiExpression(predicate, giaExp);
-            //     predicate = s => s.Gia <= giaDen;
-            // }
 
             return predicate;
         }
         public static Expression<Func<ThucDon, bool>> NoiExpression(Expression<Func<ThucDon, bool>> exp1, Expression<Func<ThucDon, bool>> exp2)
         {
             ParameterExpression parameter = Expression.Parameter(typeof(ThucDon), "t");
-
             Expression body = Expression.AndAlso(exp1.Body, exp2.Body);
             var newlambda = Expression.Lambda<Func<ThucDon, bool>>(body, parameter);
             return newlambda;
